5b1bd0a95fb73587963bd99f9500965a150785ab
Ansab Naif <ansab20556@gmail.com>
2025-07-27 09:21:51 +0300

    add script humbergar to make this web page reponsive

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#hamburger").addEventListener("click", ()=>{
+    document.querySelector(".menu-items").classList.toggle("hidden")
+    document.querySelector(".menu-items").classList.add("mt-20")
+});
